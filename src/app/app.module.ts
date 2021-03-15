31b7658ff08281c2002db91dc66bbab7bd41bc56
Aleksey Zolotuhin <jakal66@gmail.com>
2021-03-15 12:31:57 +0500

    Generate and set up hero-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
 @NgModule({
   declarations: [
     AppComponent,
